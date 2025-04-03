590718013b7d04ada6d2c1e43f0bd70ba61d4d9c
Nontawat Wuttikam <nontawatwuttikam@gmail.com>
2025-04-03 15:23:39 +0700

    modify shell scripts and configs

--- export_eval_hpatches.sh
+++ export_eval_hpatches.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # set the directory name of this evaluation.
-dataName="v13_chkpt20300_allim"
+# dataName="eval_proxy_superpoint_v15_lowlight_lr0.005_noLRsched_gradac16_4500"
+dataName="eval_v15_welllit_externalImageHomoadapt"
 
 # proxyopt config path
-proxyoptConfig="../ProxyOpt/train_configs/v13.yaml"
+proxyoptConfig="../ProxyOpt/train_configs/v15.yaml"
 
 # optimized hype path, specify "original" if wanted original hype rather than optimized hype. according to proxyopt config file.
-stage2Checkpoint="logs/superpoint_coco_proxyoptv13_allim/proxyopt_checkpoints/hype_checkpoint_20300.npy"
+stage2Checkpoint="/home/boat/proxyISP/pytorch-superpoint/logs/train_v15_welllit_expoLR_externalImageHomoadapt/proxyopt_checkpoints/checkpoint_5000.pkl"
+# stage2Checkpoint="original"
+
+hpatchesSeqPrefix="wl" # ll, wl, sl
 
 rm -rf datasets/HPatches
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate proxyopt
-python make_hpatches.py $proxyoptConfig $stage2Checkpoint
+python make_hpatches.py $proxyoptConfig $stage2Checkpoint $hpatchesSeqPrefix
 conda deactivate
 
 conda activate py36-sp
-python export.py export_descriptor  configs/magicpoint_repeatability_heatmap.yaml $dataName
+python export.py export_descriptor  configs/magicpoint_repeatability_heatmap_proxyopt.yaml $dataName
 python evaluation.py logs/$dataName/predictions --repeatibility --outputImg --homography --plotMatching
--- train_proxy_stage2.sh
+++ train_proxy_stage2.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-python train4.py train_joint configs/superpoint_coco_train_heatmap_proxyopt.yaml superpoint_coco_proxyoptv13_allim --eval --debug
+EXPER=train_v15_welllit_expoLR_externalImageHomoadapt
+CONFIG=configs/superpoint_coco_train_heatmap_proxyopt.yaml
+
+# mkdir logs/$EXPER
+# cp $CONFIG logs/$EXPER
+# mv logs/$EXPER/$(basename "${CONFIG}") logs/$EXPER/train_config.yaml
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate py36-sp
+
+python train4.py train_joint $CONFIG $EXPER --eval --debug
